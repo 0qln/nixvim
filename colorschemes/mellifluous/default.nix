25618f958ff0f4803165562c4e674ea363c9ab59
0qln <0qln@proton.me>
2026-01-06 23:53:38 +0100

    fix colorscheme/mellifluous

--- colorschemes/mellifluous/default.nix
+++ colorschemes/mellifluous/default.nix
@@ -1,22 +1,9 @@
 { lib, ... }:
-let
-  inherit (lib.nixvim) toLuaObject;
-in
-lib.nixvim.plugins.mkNeovimPlugin {
+with lib;
+nixvim.plugins.mkNeovimPlugin {
   name = "mellifluous";
   isColorscheme = true;
   package = "mellifluous-nvim";
-
+  colorscheme = "mellifluous";
   maintainers = [ ];
-
-  settingsOptions = {
-  };
-
-  extraConfig = cfg: {
-    colorschemes.mellifluous.luaConfig.content = ''
-      local mellifluous = require("mellifluous")
-      mellifluous.setup(${toLuaObject cfg.settings})
-      mellifluous.colorscheme()
-    '';
-  };
 }
